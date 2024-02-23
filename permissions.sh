c5e4cb0d36033d2a98196702ea6a9ef07ffffd0f
CaptainZiboo <captainziboo@gmail.com>
2024-02-23 08:40:52 +0100

    fix: permissions script

--- permissions.sh
+++ permissions.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 # Set permissions and ownership for Mosquitto data folder
-sudo docker compose exec mqtt chmod -R 777 /mosquitto/config
-sudo docker compose exec mqtt chmod -R 777 /mosquitto/data
-sudo docker compose exec mqtt chmod -R 777 /mosquitto/log
-sudo docker compose exec mqtt chown -R 1883:1883 /mosquitto/config
-sudo docker compose exec mqtt chown -R 1883:1883 /mosquitto/data
-sudo docker compose exec mqtt chown -R 1883:1883 /mosquitto/log
+sudo chown -R 1000:1000 /data/mosquitto
+sudo chmod -R 777 /data/mosquitto
 
 # Set permissions and ownership for Node-RED data folder
-sudo docker compose exec nodered chmod -R 777 /data
-sudo docker compose exec nodered chown -R 1000:1000 /data
+sudo chown -R 1000:1000 ./data/nodered
+sudo chmod -R 755 ./data/nodered
+
