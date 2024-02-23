e429b567fa6283da26c9d1c1781ae21ca32b027b
CaptainZiboo <captainziboo@gmail.com>
2024-02-23 08:58:39 +0100

    fix: permissions path

--- permissions.sh
+++ permissions.sh
@@ -5,8 +5,8 @@
 if [ ! -d "./data/mosquitto" ]; then
     mkdir -p ./data/mosquitto
 fi
-sudo chown -R 1000:1000 /data/mosquitto
-sudo chmod -R 777 /data/mosquitto
+sudo chown -R 1000:1000 ./data/mosquitto
+sudo chmod -R 777 ./data/mosquitto
 
 # Set permissions and ownership for Node-RED data folder
 if [ ! -d "./data/nodered" ]; then
