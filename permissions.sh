5ec152c983c07c4f80905752ba38c7f181123ba2
CaptainZiboo <captainziboo@gmail.com>
2024-02-23 08:41:53 +0100

    fix: permissions script

--- permissions.sh
+++ permissions.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # Set permissions and ownership for Mosquitto data folder
+if [ ! -d "./data/mosquitto" ]; then
+    mkdir ./data/mosquitto
+fi
 sudo chown -R 1000:1000 /data/mosquitto
 sudo chmod -R 777 /data/mosquitto
 
 # Set permissions and ownership for Node-RED data folder
+if [ ! -d "./data/nodered" ]; then
+    mkdir ./data/nodered
+fi
 sudo chown -R 1000:1000 ./data/nodered
 sudo chmod -R 755 ./data/nodered
 
